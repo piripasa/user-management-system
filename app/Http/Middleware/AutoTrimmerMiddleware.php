7732bd01f36c7ca809864ea6e47ee85e0d463cef
Zaman <zaman.pasa@shohoz.net>
2019-03-12 17:55:59 +0600

    cors & trimmer middleware

---
+++ app/Http/Middleware/AutoTrimmerMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class AutoTrimmerMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $request->merge(array_map(function ($item) {
+            if (is_string($item)) {
+                return trim($item);
+            }
+            return $item;
+        }, $request->all()));
+
+        return $next($request);
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -63,6 +63,11 @@ $app->singleton(
 //    App\Http\Middleware\ExampleMiddleware::class
 // ]);
 
+$app->middleware([
+    App\Http\Middleware\CorsMiddleware::class,
+    App\Http\Middleware\AutoTrimmerMiddleware::class
+]);
+
 // $app->routeMiddleware([
 //     'auth' => App\Http\Middleware\Authenticate::class,
 // ]);
