4c7b84305334ccd3aeed4673403e5525b24fb3a4
Zaman-A-Piri Pasa <pollobcse05@gmail.com>
2019-03-13 00:33:55 +0600

    install script updated

--- start.sh
+++ start.sh
@@ -2,16 +2,17 @@
 
 set -e
 #sh ./scripts/docker.sh
- [ -f ".env.docker" ] || $(echo Please make an .env.docker file --env=docker; exit 1)
-export $(cat .env.docker | grep -v ^# | xargs);
+# [ -f ".env.docker" ] || $(echo Please make an .env.docker file --env=docker; exit 1)
+#export $(cat .env.docker | grep -v ^# | xargs);
+sudo docker-compose down
 echo Starting services
 sudo docker-compose up -d
-echo Host: 127.0.0.1
-until sudo docker-compose exec mysql mysql -h mysql -u $DB_USERNAME -p$DB_PASSWORD -D $DB_DATABASE --silent -e "show databases;"
-do
-  echo "Waiting for database connection..."
-  sleep 5
-done
+#echo Host: 127.0.0.1
+#until sudo docker-compose exec mysql mysql -h mysql -u $DB_USERNAME -p$DB_PASSWORD -D $DB_DATABASE --silent -e "show databases;"
+#do
+#  echo "Waiting for database connection..."
+#  sleep 5
+#done
 sudo docker-compose exec php cp /var/www/html/.env.example /var/www/html/.env
 echo "Installing dependencies"
 sudo docker-compose exec php composer install
